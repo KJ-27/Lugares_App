bd4c75b0e63bbfddc8752c919c4d675b0525b55f
KJ_27 <cespedeskendall@hotmail.com>
2022-06-15 18:47:56 -0600

    List Lugares
    Data can be see it

--- app/src/main/java/com/example/lugares/ui/lugar/LugarFragment.kt
+++ app/src/main/java/com/example/lugares/ui/lugar/LugarFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.lugares.R
+import com.example.lugares.adapter.LugarAdapter
 import com.example.lugares.databinding.FragmentLugarBinding
 import com.example.lugares.viewmodel.LugarViewModel
 
@@ -27,6 +29,17 @@ class LugarFragment : Fragment() {
 
         binding.addLugarFabButton.setOnClickListener{
             findNavController().navigate(R.id.action_nav_lugar_to_addLugarFragment2)
+        }
+
+        // Activar el ReciclerView
+        val lugarAdapter = LugarAdapter()
+        val reciclador = binding.recyclerView
+        reciclador.adapter = lugarAdapter
+        reciclador.layoutManager = LinearLayoutManager(requireContext())
+
+        lugarViewModel = ViewModelProvider(this)[LugarViewModel::class.java]
+        lugarViewModel.getAllData.observe(viewLifecycleOwner) { lugares ->
+            lugarAdapter.setData(lugares)
         }
 
         return binding.root
