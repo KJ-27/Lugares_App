b9faaabece06712fb81e47bea11f1d615c882513
KJ_27 <cespedeskendall@hotmail.com>
2022-06-18 14:50:42 -0600

    Delete Done
    All working as expected

--- app/src/main/java/com/example/lugares/ui/lugar/UpdateLugarFragment.kt
+++ app/src/main/java/com/example/lugares/ui/lugar/UpdateLugarFragment.kt
@@ -1,10 +1,9 @@
 package com.example.lugares.ui.lugar
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -39,7 +38,21 @@ class UpdateLugarFragment : Fragment() {
 
         binding.btAdd.setOnClickListener{ updateLugar() }
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.delete_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Se es eliminar
+        if (item.itemId == R.id.menu_delete){
+            deleteLugar()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     private fun updateLugar() {
@@ -53,10 +66,27 @@ class UpdateLugarFragment : Fragment() {
             lugarViewModel.updateLugar(lugar)
             Toast.makeText(requireContext(),getString(R.string.msgLugarActualizado),Toast.LENGTH_LONG,).show()
 
-            findNavController().navigate(R.id.action_addLugarFragment_to_nav_lugar3)
+            findNavController().navigate(R.id.action_updateLugarFragment3_to_nav_lugar)
         } else {
             Toast.makeText(requireContext(),getString(R.string.msgFaltanDatos),Toast.LENGTH_LONG,).show()
         }
+    }
+
+    private fun deleteLugar() {
+        val builder = AlertDialog.Builder(requireContext())
+
+        builder.setPositiveButton(getString(R.string.si)) { _,_ ->
+            lugarViewModel.deleteLugar(args.lugar)
+
+            Toast.makeText(requireContext(),getString(R.string.msgDeleted) + " ${args.lugar.nombre}",Toast.LENGTH_LONG,).show()
+
+            findNavController().navigate(R.id.action_updateLugarFragment3_to_nav_lugar)
+        }
+
+        builder.setNegativeButton(getString(R.string.no)) { _,_ -> }
+        builder.setTitle(R.string.msgDeleted)
+        builder.setMessage(getString(R.string.confirmarEliminar)+ " ${args.lugar.nombre}?")
+        builder.create().show()
     }
 
     private fun validos(nombre: String, correo: String, telefono: String, web: String): Boolean {
